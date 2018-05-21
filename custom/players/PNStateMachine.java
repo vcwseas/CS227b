acfca43747e31411a892cb33847ed4b3042c2ed8
kaanertas <38227810+kaanertas@users.noreply.github.com>
2018-05-20 17:25:16 -0700

    Add files via upload

--- custom/players/PNStateMachine.java
+++ custom/players/PNStateMachine.java
@@ -1,5 +1,6 @@
 package ass1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -13,7 +14,10 @@ import org.ggp.base.util.propnet.architecture.components.Not;
 import org.ggp.base.util.propnet.architecture.components.Or;
 import org.ggp.base.util.propnet.architecture.components.Proposition;
 import org.ggp.base.util.propnet.architecture.components.Transition;
-import org.ggp.base.util.propnet.factory.OptimizingPropNetFactory;;
+import org.ggp.base.util.propnet.factory.OptimizingPropNetFactory;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;;
 
 public class PNStateMachine {
 
@@ -22,10 +26,6 @@ public class PNStateMachine {
 	public PNStateMachine(List<Gdl> rules) throws InterruptedException {
 		PN = OptimizingPropNetFactory.create(rules, true);
 	}
-
-
-
-
 
 	private void markBases(Set<GdlSentence> sentences){
 		for (GdlSentence sent: PN.getBasePropositions().keySet()){
@@ -39,7 +39,45 @@ public class PNStateMachine {
 		}
 	}
 
+	private void clearPN() {
+		for (Proposition p: PN.getPropositions()) {
+			p.setValue(false);
+		}
+	}
 
+	public List<Move> getLegals(Role r,MachineState s) {
+		markBases(s.getContents());
+		Set<Proposition> legals=PN.getLegalPropositions().get(r);
+		List<Move> moves=new ArrayList<Move>();
+		for (Proposition p: legals) {
+			if (propMark((Component) p)) moves.add(new Move(p.getName().getBody().get(1)));
+		}
+		clearPN();
+		return moves;
+	}
+
+	public MachineState getNextState(List<Move> moves, MachineState s) {
+
+		//TODO: Implement
+		clearPN();
+		return s;
+	}
+
+	public int getReward(Role r, MachineState s) {
+		int goal=0;
+		markBases(s.getContents());
+		Set<Proposition> goals=PN.getGoalPropositions().get(r);
+		for (Proposition p: goals) {
+			if (propMark(p)) goal = Integer.parseInt(p.getName().getBody().get(1).toString());
+		}
+		clearPN();
+		return goal;
+	}
+
+	public boolean findTerminalp(MachineState s) {
+		markBases(s.getContents());
+		return PN.getTerminalProposition().getValue();
+	}
 
 	private boolean propMark(Component c){
 		if (c instanceof Proposition){
